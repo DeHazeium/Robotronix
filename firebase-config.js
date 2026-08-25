fc9f674dae336fd7573dc02e71125f5604d277f9
M.Irfan <f4nzamv05@gmail.com>
2026-08-25 14:14:03 +0800

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// Firebase Console > Project settings > Your apps > Web app > SDK setup.
+// Replace every YOUR_... value with the values from your Firebase project.
+export const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+  databaseURL: "https://YOUR_PROJECT_ID-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT_ID.firebasestorage.app",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID"
+};
